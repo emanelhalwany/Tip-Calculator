5ada054547717dd5cac2d915c162eadfc75c5e57
Eman <emanelhalwany1@.com>
2020-01-31 22:26:22 +0200

    edit err message appering , used sutible class names and add if function in clacTip function

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 
-//   validtion function 
+//   validtion function for bill input 
 function validation() {
-  var inpObj = document.getElementById("id1");
+  var inpObj = document.getElementById("bill");
   if (!inpObj.checkValidity()) {
-    document.getElementById("demo").innerHTML = inpObj.validationMessage;
-  
+    document.getElementById("err-validation").innerHTML = inpObj.validationMessage;
+  } else {
+    document.getElementById("err-validation").innerHTML = "";
+  }
 }
-}
-//   validtion function 
+//   validtion function for people input
 function validation2() {
-  var inpObj = document.getElementById("id2");
+  var inpObj = document.getElementById("num-people");
   if (!inpObj.checkValidity()) {
-    document.getElementById("demo2").innerHTML = inpObj.validationMessage;
-
-}
+    document.getElementById("err-validation2").innerHTML = inpObj.validationMessage;
+  }else {
+    document.getElementById("err-validation2").innerHTML = "";
+  }
 }
 
 function calcTip() {
-  
-  var bill = document.getElementById('id1').value;
-  var numOfPeople = document.getElementById('id2').value;
-  var quality = document.getElementById('serviceSeclesction').value;
-  console.log(numOfPeople);
-  console.log(bill);
-  console.log(quality);
-  var tipAmount = (bill / numOfPeople) * quality/100;
 
-  document.getElementById('tip-amount').innerHTML = "Tip Amount";
-  document.getElementById('each').innerHTML = "Each";
-  document.getElementById('tip').innerHTML = "$" + tipAmount;
+  var bill = document.getElementById('bill').value;
+  var numOfPeople = document.getElementById('num-people').value;
+  var quality = document.getElementById('serviceSeclesction').value;
+  var tipAmount = (bill / numOfPeople) * quality / 100;
+  
+  if (isNaN(tipAmount) || tipAmount == Infinity){
+    document.getElementById('tip').innerHTML = "it's wrong , please try again with another values";
+  }else{
+    document.getElementById('tip-amount').innerHTML = "Tip Amount";
+    document.getElementById('each').innerHTML = "Each";
+    document.getElementById('tip').innerHTML = "$" + tipAmount;
+  }
 
 }
